780163405d88f5ef32f25e435f53a14d206cb32f
Sam Kellett <sam@wavestore.com>
2014-03-19 17:41:55 +0000

    Updated .xmonad configuration

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,20 +3,35 @@ import XMonad
 import Control.Monad
 import qualified XMonad.StackSet as W
 
-import XMonad.Util.Run(spawnPipe)
-
 -- definitions
 myTerminal = "urxvt"
+myModMask = mod4Mask
+
+myWorkspaces = ["1consoles", "2qt", "3chat", "4web", "5email", "6media", "7other"]
 
 myStartupHook = do
   spawn "~/.xmonad/startup.py"
 
-m4 = mod4Mask;
+myManageHook = composeAll . concat $ [
+    [ className =? "urxvt" --> viewShift "1consoles" ],
+    [ className =? "qtcreator" --> viewShift "2qt" ],
+    [ className =? "hipchat" --> viewShift "3chat" ],
+    [ className =? "google-chrome" --> viewShift "4web" ] -- ,
+--    [ className =? "" --> viewShift "5email" ],
+--    [ className =? "" --> viewShift "6media" ]
+  ]
+  where
+    viewShift = doF . liftM2 (.) W.greedyView W.shift
+
 main = do
   xmonad $ defaultConfig {
-    modMask = m4,
+    modMask = myModMask,
     borderWidth = 2,
+
     terminal = myTerminal,
+    workspaces = myWorkspaces,
+
+    manageHook = myManageHook,
     startupHook = myStartupHook
   }
 
